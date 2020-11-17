74299a86155079dafd877183721fc9e9a1e909ab
Hsin-Cheng Chao <olala7846@gmail.com>
2020-11-16 22:12:19 -0700

    Use drop down GUI, display both image and camera with different view port

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as dat from 'dat.gui';
 import {
   Geometry, WebGLRenderer, Scene,
   PerspectiveCamera, DirectionalLight, Object3D,
-  Material, GridHelper, AxesHelper, Color, PlaneGeometry, PointsMaterial, Matrix4, Vector3, BoxGeometry, MeshBasicMaterial, WireframeGeometry, LineSegments, BooleanKeyframeTrack, CameraHelper
+  Material, GridHelper, AxesHelper, Color, PlaneGeometry, PointsMaterial, Matrix4, Vector3, BoxGeometry, MeshBasicMaterial, WireframeGeometry, LineSegments, BooleanKeyframeTrack, CameraHelper, ImageLoader
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { PCDLoader } from 'three/examples/jsm/loaders/PCDLoader';
@@ -33,18 +33,12 @@ interface LaserConfig {
   rear: boolean,
 };
 
-interface CameraConfig {
-  front: boolean,
-  frontLeft: boolean,
-  frontRight: boolean,
-  sideLeft: boolean,
-  sideRight: boolean,
-};
-
 // Chrysler Pacifica has size 204″ L x 80″ W x 70″ H
 const CAR_X = 5.18;
 const CAR_Y = 2.03;
 const CAR_Z = 1.78;
+
+const HELPER_CAMERA_WIDTH = 300;
 
 let laserUiControl: LaserConfig = {
   top: true,
@@ -61,24 +55,9 @@ let laserEnabled = new Map<LaserName, boolean>([
   [LaserName.REAR, true],
 ]);
 
-let cameraUiControl: CameraConfig = {
-  front: false,
-  frontLeft: false,
-  frontRight: false,
-  sideLeft: false,
-  sideRight: false,
-};
-
-let cameraEnabled = new Map<CameraName, boolean>([
-  [CameraName.FRONT, false],
-  [CameraName.FRONT_LEFT, false],
-  [CameraName.FRONT_RIGHT, false],
-  [CameraName.SIDE_LEFT, false],
-  [CameraName.SIDE_RIGHT, false],
-]);
-
 interface HelperControl {
   axesHelper: boolean,
+  cameraName: string,
 };
 
 const POINT_SIZE = 0.05;
@@ -96,7 +75,6 @@ let car: Object3D;
 
 // Controllers
 let axesHelper: AxesHelper;
-let showAxesHelper = true;
 const allLaserNames = [
   LaserName.TOP,
   LaserName.FRONT,
@@ -112,6 +90,8 @@ laserColor.set(LaserName.SIDE_RIGHT, 0x03a1fc);
 laserColor.set(LaserName.REAR, 0xfcf003);
 let laserObjectMap = new Map<LaserName, Object3D>();
 let cameraHelperMap = new Map<CameraName, CameraHelper>();
+let cameraMap = new Map<CameraName, PerspectiveCamera>();
+let helperCameraName: CameraName | "None" = CameraName.FRONT_RIGHT;
 
 function fillScene() {
   scene = new THREE.Scene();
@@ -157,45 +137,73 @@ function fillScene() {
 
 function setupGui() {
   helperControl = {
-    axesHelper: showAxesHelper,
+    axesHelper: true,
+    cameraName: "None",
   };
 
   let gui = new dat.GUI();
-  let sensers = gui.addFolder('Sensers');
+  let sensors = gui.addFolder('Sensers');
   Object.keys(laserUiControl).forEach((laserName: string) => {
-    sensers.add(laserUiControl, laserName).name(laserName);
+    sensors.add(laserUiControl, laserName).name(laserName);
   });
 
-  let cameras = gui.addFolder('Cameras');
-  Object.keys(cameraUiControl).forEach((cameraName: string) => {
-    cameras.add(cameraUiControl, cameraName).name(cameraName);
-  });
+  sensors.add(helperControl, 'cameraName', [
+    "None",
+    CameraName.FRONT,
+    CameraName.FRONT_LEFT,
+    CameraName.FRONT_RIGHT,
+    CameraName.SIDE_LEFT,
+    CameraName.SIDE_RIGHT,
+  ])
+    .name('Camera')
+    .onChange((selectedCameraName) => {
+      helperCameraName = selectedCameraName;
+      // Display select camera image
+      drawImage(selectedCameraName);
+      cameraHelperMap.forEach((cameraHelper, cameraName, map) => {
+        if (cameraName == selectedCameraName) {
+          scene.add(cameraHelper);
+        } else {
+          scene.remove(cameraHelper);
+        }
+      });
+    });
+  // Initial image draw.
+  drawImage(helperCameraName);
 
   let helpers = gui.addFolder('Helpers');
-  Object.keys(helperControl).forEach((key: string) => {
-    helpers.add(helperControl, key).name(key);
-  });
+  helpers.add(helperControl, 'axesHelper')
+    .name('Axes Helper')
+    .onChange((enabled) => {
+      if (enabled) {
+        scene.add(axesHelper);
+      } else {
+        scene.remove(axesHelper);
+      }
+    });
+
 }
 
 function init() {
-	// For grading the window is fixed in size; here's general code:
 	let canvasWidth = window.innerWidth;
 	let canvasHeight = window.innerHeight;
-	var canvasRatio = canvasWidth / canvasHeight;
+  let mainPortRatio = (canvasWidth - HELPER_CAMERA_WIDTH)/ canvasHeight;
 
 	// RENDERER
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	// renderer.gammaInput = true;
 	// renderer.gammaOutput = true;
 	renderer.setSize(canvasWidth, canvasHeight);
-	renderer.setClearColor( DARK_SKY_COLOR, 1.0 );
+  renderer.setClearColor( DARK_SKY_COLOR, 1.0 );
+
+  renderer.autoClear = false;
 
   // CAMERA
   // Waymo dataset hehicle frame
   //  The x-axis is positive forwards, y-axis is positive to the left,
   //  z-axis is positive upwards. A vehicle pose defines the transform
   //  from the vehicle frame to the global frame.
-  camera = new THREE.PerspectiveCamera( 55, canvasRatio, 2, 10000 );
+  camera = new THREE.PerspectiveCamera( 55, mainPortRatio, 2, 10000 );
   camera.position.set(10, -20, 5);
   camera.up.set(0, 0, 1);
   camera.lookAt(0, 0, 0);
@@ -214,24 +222,17 @@ function init() {
 }
 
 function addToDOM() {
-	var container = document.getElementById('container');
+	var container = document.getElementById('container3d');
 	var canvas = container.getElementsByTagName('canvas');
 	if (canvas.length>0) {
 		container.removeChild(canvas[0]);
 	}
-	container.appendChild( renderer.domElement );
+  container.appendChild( renderer.domElement );
 }
 
-function drawHelpers() {
-  if (showAxesHelper) {
-    if (axesHelper === undefined) {
-      axesHelper = new AxesHelper(2);
-      axesHelper.position.set(0, 0, 0);
-    }
-    scene.add(axesHelper);
-  } else {
-    scene.remove(axesHelper);
-  }
+function initHelpers() {
+  axesHelper = new AxesHelper(2);
+  axesHelper.position.set(0, 0, 0);
 }
 
 function animate() {
@@ -262,11 +263,11 @@ function fetchCameraCalibration() {
     return response.json();
   }).then((json) => {
     cameraCalibration = json;
-    drawCameraFrustrum();
+    initCameraHelpers();
   });
 }
 
-function drawCameraFrustrum() {
+function initCameraHelpers() {
   if (cameraCalibration === undefined) {
     console.log('waiting from camera calibration data...');
     return;
@@ -274,12 +275,11 @@ function drawCameraFrustrum() {
 
   let frustrums: Array<any> = cameraCalibration['frustrums'];
   frustrums.forEach((cameraData: any) => {
-    drawSingleCamera(cameraData);
+    initSingleCameraHelper(cameraData);
   });
-  // drawSingleCamera(frustrums[0]);
 }
 
-function drawSingleCamera(cameraData: any) {
+function initSingleCameraHelper(cameraData: any) {
   let cameraName = cameraData.name;
   let fv, fu, cv, cu;
   let k1, k2, p1, p2, k3
@@ -290,37 +290,25 @@ function drawSingleCamera(cameraData: any) {
   // Matrix4D.elements stores data in column major form, we need to transpose it
   transform = transform.transpose();
 
-
-  let aspectRatio = fv/fu;
+  // TODO: camera aspect ratio seems a bit off
+  let aspectRatio = fu/fv;
   // all camera images has width 1920
   let fov = Math.atan2(fv, 960.0) / Math.PI * 180;
-  let perspectiveCamera = new THREE.PerspectiveCamera(fov, aspectRatio, 0.1, 20);
+  let perspectiveCamera = new THREE.PerspectiveCamera(fov, aspectRatio, 0.5, 30);
 
   // Waymo camera seems to point to +x axis instead of -Z
-  perspectiveCamera.lookAt(1, 0, 0);
   perspectiveCamera.up.set(0, 0, 1);
+  perspectiveCamera.lookAt(1, 0, 0);
   let carCamera = new Object3D();
   carCamera.add(perspectiveCamera);
   carCamera.matrixAutoUpdate = false;
   carCamera.applyMatrix4(transform);
   carCamera.updateMatrixWorld(true);
 
+  cameraMap.set(cameraName, perspectiveCamera);
+
   let helper = new THREE.CameraHelper(perspectiveCamera);
   cameraHelperMap.set(cameraName, helper);
-}
-
-function updateCamera(cameraName: CameraName, enabled: boolean) {
-  if (cameraEnabled.get(cameraName) == enabled) {
-    return;
-  }
-
-  let helper = cameraHelperMap.get(cameraName);
-  if (enabled) {
-    scene.add(helper);
-  } else {
-    scene.remove(helper);
-  }
-  cameraEnabled.set(cameraName, enabled);
 }
 
 function render() {
@@ -331,26 +319,57 @@ function render() {
   updateLaser(LaserName.SIDE_RIGHT, laserUiControl.sideRight);
   updateLaser(LaserName.REAR, laserUiControl.rear);
 
-  updateCamera(CameraName.FRONT, cameraUiControl.front);
-  updateCamera(CameraName.FRONT_LEFT, cameraUiControl.frontLeft);
-  updateCamera(CameraName.FRONT_RIGHT, cameraUiControl.frontRight);
-  updateCamera(CameraName.SIDE_RIGHT, cameraUiControl.sideLeft);
-  updateCamera(CameraName.SIDE_RIGHT, cameraUiControl.sideRight);
+	var delta = clock.getDelta();
+  cameraControls.update();
 
-  if (helperControl.axesHelper !== showAxesHelper) {
-    showAxesHelper = helperControl.axesHelper;
-    drawHelpers();
+  let helperWidth = HELPER_CAMERA_WIDTH;
+
+  renderer.clear();
+
+  // Set view port to render multiple cameras
+  renderer.setViewport(0, 0, window.innerWidth - helperWidth, window.innerHeight);
+  renderer.render(scene, camera);
+
+  // Draw helper camera
+  if (!!helperCameraName && helperCameraName !== "None") {
+    let helperCamera = cameraMap.get(helperCameraName);
+    let helperHeight = helperWidth / helperCamera.aspect;
+    renderer.setViewport(window.innerWidth - helperWidth, 300, helperWidth, helperHeight);
+    renderer.render(scene, helperCamera);
+  }
+}
+
+function drawImage(cameraName: string) {
+  let canvas = document.getElementById('canvas2d') as HTMLCanvasElement;
+  let context2d = canvas.getContext('2d');
+  // always clear for redraw
+  context2d.clearRect(0, 0, canvas.width, canvas.height);
+  if (cameraName == "None") {
+    return;
   }
 
-	var delta = clock.getDelta();
-	cameraControls.update();
-  renderer.render(scene, camera);
+  let imagePath = `/python/out/1.${cameraName}.png`;
+  let loader = new ImageLoader();
+  // dw, dh for destination width and height
+  loader.load(imagePath, (image) => {
+    // sw, sh for source width and source height;
+    const sw = image.width;
+    const sh = image.height;
+    const dw = HELPER_CAMERA_WIDTH;
+    const dh = dw * sh / sw;
+    // Scale image and put it in lower left corner.
+    context2d.drawImage(image, 0, 0, sw, sh, canvas.width - dw, 0, dw, dh);
+  },
+  undefined,
+  (error) => {
+    console.error('Error loading image');
+  });
 }
 
 fetchCameraCalibration();
 init();
 fillScene();
+initHelpers();
 setupGui();
-drawHelpers();
 addToDOM();
 animate();
