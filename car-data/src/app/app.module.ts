66b96dc9d0533d9a2a89cf72dd02dd65d57e8568
Hsin-Cheng Chao <olala7846@gmail.com>
2021-01-30 21:34:20 -0700

    Add support view UI

--- car-data/src/app/app.module.ts
+++ car-data/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MainViewComponent } from './main-view/main-view.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 import { SupportViewComponent } from './support-view/support-view.component';
-import { MainViewComponent } from './main-view/main-view.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { MainViewComponent } from './main-view/main-view.component';
     BrowserAnimationsModule,
     BrowserModule,
     FlexLayoutModule,
+    MatCardModule,
+    MatCheckboxModule,
     MatIconModule,
+    MatSelectModule,
     MatToolbarModule,
   ],
   providers: [],
--- car-data/src/app/support-view/support-view.component.ts
+++ car-data/src/app/support-view/support-view.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupportViewComponent implements OnInit {
 
+  cameraNames = [
+    'FRONT',
+    'FRONT_LEFT',
+    'FRONT_RIGHT',
+    'SIDE_LEFT',
+    'SIDE_RIGHT',
+  ];
+  camera: string;
+
+
   constructor() { }
 
   ngOnInit(): void {
